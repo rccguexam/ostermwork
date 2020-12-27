b5cc10152ff3d7aa9b034e0967476524a5870e5e
neel05 <neelrana6@gmail.com>
2020-12-27 22:41:30 +0530

    updated ass-2

--- MCA-3_09/practicles/09_q2.sh
+++
@@ -1,6 +0,0 @@
-#print the command line argument and print the total passed argument 
-
-echo "There are $# arguments specified at the command line "
-echo "The argument supplied are : $*"
-echo "The first argument is : $1"
-echo "The second argument is : $2"
